12339669b5f0e837e12058e2d3505624f914db24
itoshun <tyuke.net@gmail.com>
2017-05-02 17:06:54 +0900

    add SplashView

--- app/src/main/java/com/example/itoshun/samplenfc/FullscreenActivity.java
+++ app/src/main/java/com/example/itoshun/samplenfc/FullscreenActivity.java
@@ -83,7 +83,7 @@ public class FullscreenActivity extends AppCompatActivity {
             return false;
         }
     };
-    
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -103,10 +103,6 @@ public class FullscreenActivity extends AppCompatActivity {
             }
         });
 
-        // Upon interacting with UI controls, delay any scheduled hide()
-        // operations to prevent the jarring behavior of controls going away
-        // while interacting with the UI.
-        findViewById(R.id.dummy_button).setOnTouchListener(mDelayHideTouchListener);
     }
 
     @Override
