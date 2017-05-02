d10d16a6c67a17b77527bf25c0e707057cbe3b0f
itoshun <tyuke.net@gmail.com>
2017-05-02 14:06:36 +0900

    add NFC instance

--- app/src/main/java/com/example/itoshun/samplenfc/MainActivity.java
+++ app/src/main/java/com/example/itoshun/samplenfc/MainActivity.java
@@ -1,13 +1,22 @@
 package com.example.itoshun.samplenfc;
 
+import android.nfc.NfcAdapter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private NfcAdapter mNfcAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Get NFC instance
+        mNfcAdapter = NfcAdapter.getDefaultAdapter(this);
     }
+
+
+
 }
