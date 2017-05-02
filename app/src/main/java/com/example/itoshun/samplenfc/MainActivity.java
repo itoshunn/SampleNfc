b75a685d894f2caf43d40169b95d7da89ff5b578
itoshun <tyuke.net@gmail.com>
2017-05-02 14:32:29 +0900

    add onResume

--- app/src/main/java/com/example/itoshun/samplenfc/MainActivity.java
+++ app/src/main/java/com/example/itoshun/samplenfc/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.itoshun.samplenfc;
 
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.nfc.NfcAdapter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -8,6 +10,10 @@ public class MainActivity extends AppCompatActivity {
 
     private NfcAdapter mNfcAdapter;
 
+    /**
+     * onCreate
+     * @param savedInstanceState
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,6 +23,24 @@ public class MainActivity extends AppCompatActivity {
         mNfcAdapter = NfcAdapter.getDefaultAdapter(this);
     }
 
+    /**
+     * onResume
+     * レジューム機能
+     */
+    @Override
+    protected void onResume() {
+        super.onResume();
 
+        // NFC Intent の作成
+        Intent intent  = new Intent(new, this.getClass());
+        intent.setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
+
+        // PendingIntent (待機状態の受け取り)
+        PendingIntent pendingIntent = PendingIntent.getActivity(
+                getApplicationContext(), 0, intent, 0);
+        // 前面で処理する
+        mNfcAdapter.enableForegroundDispatch(this, pendingIntent, null, null);
+
+    }
 
 }
